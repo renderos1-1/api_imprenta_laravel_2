954afa25a5087689100c3656efbd7ff0db7aac47
Rolando Antonio Zelaya Bulnes <20235007@esen.edu.sv>
2024-11-29 11:25:08 -0600

    graficos template

--- resources/views/estadisticas.blade.php
+++ resources/views/estadisticas.blade.php
@@ -4,16 +4,18 @@
     <div class="grid-container">
         <!-- Cada div representa un "item" dentro de la cuadrícula -->
         <div class="grid-item">Item 1</div>
-        <div class="grid-item">Item 2</div>
-        <div class="grid-item">Item 3</div>
-        <div class="grid-item">Item 4</div>
+        <div class="grid-item"><img src="../../../api_imprenta_laravel_2/public/img/Logo_Gobierno.png" alt="logo"></div>
+        <div class="grid-item">
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium at autem debitis, dignissimos
+                doloremque dolorum, ea, fuga fugit incidunt officia quis quo saepe soluta totam voluptates! Accusamus
+                eligendi modi vel.
+            </div>80</div>
+        <div class="grid-item">
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis cum explicabo fuga, iusto
+                minus nemo nobis numquam officia officiis pariatur qui ratione suscipit tenetur ullam ut veniam
+                voluptatum. Dignissimos?
+            </div>00</div>
         <div class="grid-item">Item 5</div>
-        <div class="grid-item">Item 6</div>
-        <div class="grid-item">Item 7</div>
-        <div class="grid-item">Item 8</div>
-        <div class="grid-item">Item 9</div>
-        <div class="grid-item">Item 10</div>
-        <div class="grid-item">Item 11</div>
-        <div class="grid-item">Item 12</div>
+        <div class="grid-item">ouyet2fiuyfqerouyfgpiur2gfogroifgyoiuqgfourufubrduroquryfouby1ofuy</div>
     </div>
 @endsection
